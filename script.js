a6b9854248f7f29f6ab597a1bf06297d0b37211d
Johnny Zhou <jonathanqyz@gmail.com>
2022-08-20 17:23:10 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+class Book {
+  constructor(title, author, pages, read) {
+      this.title = form.title.value; 
+      this.author = form.author.value; 
+      this.pages = form.pages.value + 'pg'; 
+      this.read = form.read.checked; 
+  }
+}
+
+let myLibrary = []
+
