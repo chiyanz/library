0c0e974f3055b78d88a7b11c5dae621dfd540a2c
Johnny Zhou <jonathanqyz@gmail.com>
2022-08-20 22:44:34 -0400

    Updated user interface design

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const addBookBtn = document.querySelector('#addBookBtn')
+const newBookBtn = document.querySelector('#newBookBtn')
+const popUpForm = document.querySelector('#pop-up')
+
+
+const addBookToLibrary = () => {
+  popUpForm.style.display = 'none'
+}
+
+addBookBtn.addEventListener('click', addBookToLibrary)
+newBookBtn.addEventListener('click', () => {
+  popUpForm.style.display = 'block'
+})
+
+
+
+
+
 class Book {
   constructor(title, author, pages, read) {
       this.title = form.title.value; 
@@ -8,4 +26,3 @@ class Book {
 }
 
 let myLibrary = []
-
