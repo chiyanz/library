79d9cc19ecbf993c5efa71df45780e41279b62ec
Johnny Zhou <jonathanqyz@gmail.com>
2022-08-21 22:14:09 -0400

    Survey form update

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const addBookBtn = document.querySelector('#addBookBtn')
 const newBookBtn = document.querySelector('#newBookBtn')
 const popUpForm = document.querySelector('#pop-up')
+const bookForm = document.querySelector('#form')
 
+let myLibrary = []
+let newBook
 
 const addBookToLibrary = () => {
   popUpForm.style.display = 'none'
+  newBook = new Book(title, author, pages,read); 
+    myLibrary.push(newBook); 
+  console.log(newBook)
 }
 
 addBookBtn.addEventListener('click', addBookToLibrary)
@@ -16,11 +22,11 @@ newBookBtn.addEventListener('click', () => {
 
 class Book {
   constructor(title, author, pages, read) {
-      this.title = form.title.value; 
-      this.author = form.author.value; 
-      this.pages = form.pages.value + 'pg'; 
-      this.read = form.read.checked; 
+      this.title = title; 
+      this.author = author; 
+      this.pages = pages + 'pg'; 
+      this.read = read; 
   }
 }
 
-let myLibrary = []
+
